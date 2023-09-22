1d735ad4ecc4056ad3c15e1c89969728e9282338
linuxfranz <fbrummer@gmail.com>
2023-09-22 17:36:13 +0200

    add gallery

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const blogCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: ({image}) => z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    images: z.array(image()),
+    descriptions: z.array(z.string())
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'blog': blogCollection,
+};
